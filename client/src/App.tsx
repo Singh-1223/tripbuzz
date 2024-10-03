8fc9646aaa92f8a527b97c24a45c8332204bf58c
Aryan Dumyan <aryandumyan@Aryans-MacBook-Air.local>
2024-10-03 19:15:34 +0530

    client folder setup

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="bg-cyan-950">
+        <Button>Click Me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
